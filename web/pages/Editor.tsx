f0e9aed14c87c6c47cdd6402f3d3d59a1b017196
Pandaroses <georgiytnimov@gmail.com>
2022-12-06 15:41:47 +0000

    guys brand new shit guys i am editor todo cursor and actual regex lmoao

--- web/pages/Editor.tsx
+++ web/pages/Editor.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext,useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { UserContext } from "../context";
-
+import ReactMarkdown from 'react-markdown'
 const Editor = () => {
   const { id } = useParams();
   const edit  = useRef<HTMLDivElement>(null);
@@ -10,13 +10,7 @@ const Editor = () => {
   const user = useContext(UserContext);
   const [note, setNote] = useState({ loading: true });
   const [status, setStatus] = useState("opened");
-  
-  const handleChange = (e) => {
-    edit.current.innerHTML = parse(e.target.innerText);
-    // edit.current.innerHTML = parse(edit.current.innerText)
-    console.log(edit.current.innerHTML);
-  }
-  
+  const [cursor,setCursor] = useState(0);
   
   useEffect(() => {
     if (!user.loading) {
@@ -38,74 +32,38 @@ const Editor = () => {
       }
     }
   }, [user]);
-
-  // const handleLines = (e) => {
-    
-  //   const sel = window.getSelection();
-  //   const node = sel.focusNode;
-  //   const offset = sel.focusOffset;
-  //   const pos = getCursor(editor.current, node, offset, { pos: 0, done: false });
-  //       editor.current.innerHTML = parse(editor.current.innerText);
-    
-  //   sel.removeAllRanges();
-  //   const range = setCursor(editor.current, document.createRange(), {
-  //     pos: pos.pos,
-  //     done: false,
-  //   });
-  //   range.collapse(true);
-  //   sel.addRange(range);
-  // };
-
-  // const getCursor = (parent, node, offset, stat) => {
-  //   if (stat.done) return stat;
-
-  //   let currentNode = null;
-  //   if (parent.childNodes.length == 0) {
-  //     stat.pos += parent.textContent.length;
-  //     console.log(parent);
-  //   } else {
-  //     for (let i = 0; i < parent.childNodes.length && !stat.done; i++) {
-  //       currentNode = parent.childNodes[i];
-  //       if (currentNode === node) {
-  //         stat.pos += offset;
-  //         stat.done = true;
-  //         return stat;
-  //       } else getCursor(currentNode, node, offset, stat);
-  //     }
-  //   }
-  //   return stat;
-  // };
-  
-  const parse = (text) => {
+   
+  handleEdit = (e) => {
+    switch(e.key) {
+    case "Backspace" : 
+        const fuck = note.content.replace(/<[^>]*>?/gm, '').slice(0,-1)
+        setNote({...note,...{content: parse(fuck)}});
+        break;
+    case "Enter" :
+          setNote({...note,...{content:note.content + "<br />"}})
+          break;
+    case "Shift" :
+      console.log("shit");
+          break;
+    case "Meta" :
+      console.log("shit");
+          break;
+     case "Alt" :
+      console.log("shit");
+          break;
+    default:
+      setNote({...note,...{content: parse(note.content + e.key)}})
+      
+  }
+  }
+   const parse = (text) => {
     return (
     text
       .replace(/\*{2}(.*?)\*{2}/gm, "**<strong>$1</strong>**") // bold
       .replace(/(?<!\*)\*(?!\*)(.*?)(?<!\*)\*(?!\*)/gm, "*<em>$1</em>*") // italic
-      // handle special characters
-      .replace(/\n/gm, "<br /> ")
-      .replace(/\t/gm, "&#9;")
   );
   };
-
-  // const setCursor = (parent, range, stat) => {
-  //   if (stat.done) return range;
-
-  //   if (parent.childNodes.length == 0) {
-  //     if (parent.textContent.length >= stat.pos) {
-  //       range.setStart(parent, stat.pos);
-  //       stat.done = true;
-  //     } else {
-  //       stat.pos = stat.pos - parent.textContent.length;
-  //     }
-  //   } else {
-  //     for (let i = 0; i < parent.childNodes.length && !stat.done; i++) {
-  //       currentNode = parent.childNodes[i];
-  //       setCursor(currentNode, range, stat);
-  //     }
-  //   }
-  //   return range;
-  // };
-
+  
   useEffect(() => {
     //probably should time this
     if (!note.loading) {
@@ -125,7 +83,7 @@ const Editor = () => {
       <title>{note.title}</title>
       <div className="flex justify-center items-center relative py-2">
         <h1
-          className="absolute left-2 text-2xl font-bold cursor-pointer hover:text-purple transition-colors"
+          className="absolute left-2 text-2xl font-bold cursor-pointer hover:text-purple transition-colors select-none"
           onClick={() => navigate("/")}
         >
           Floppa Notes
@@ -139,22 +97,16 @@ const Editor = () => {
         >
           {note.title}
         </p>
-        <p className="text-lightgrey ml-2 text-sm">{status}</p>
+        <p className="text-lightgrey ml-2 text-sm select-none">{status}</p>
       </div>
+    
+      <div className="resize-none outline-none bg-grey p-4 text-base flex-1 overflow-none" tabIndex={0} onKeyDown={(e) => handleEdit(e)} dangerouslySetInnerHTML={{__html: note.content}} /> 
       {/* <textarea
         value={note.content}
         onChange={(e) => {setNote({ ...note, ...{ content: e.target.value } })}
         className="resize-none outline-none bg-grey  p-4 text-base flex-1"
       /> */}
-      <div
-        ref={edit}
-        className="resize-none outline-none bg-grey p-4 text-base flex-1  w-fill overflow-y-auto overflow-x-none whitespace-pre "
-        contentEditable="true"
-        onInput={(e) => handleChange(e)}
-        suppressContentEditableWarning={true}
-    
-      ></div>
-    </div>
+               </div>
   );
 };
 
